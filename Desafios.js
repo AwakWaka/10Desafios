57f71cf4b3268bdfdb6dde178b2fa2a9e1d1adef
Kawa M <79219024+AwakWaka@users.noreply.github.com>
2024-01-23 17:52:11 -0300

    Desafios.js

---
+++ Desafios.js
@@ -0,0 +1,16 @@
+alert ("Boas vindas ao nosso site!");
+
+let nome = Lua;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let idade = 18;
+
+alert("Erro! Preencha todos os campos")
+
+nome = prompt("Por gentileza informe o seu Nome.")
+
+idade == prompt("Por gentileza informe a sua idade")
+
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
